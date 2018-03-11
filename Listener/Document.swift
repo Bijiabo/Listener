93e9d9b930aadfb0180fe009472185f3f0d7f98e
bijiabo <bijiabo@gmail.com>
2018-03-11 10:46:33 +0800

    Initial Commit

---
+++ Listener/Document.swift
@@ -0,0 +1,22 @@
+//
+//  Document.swift
+//  Listener
+//
+//  Created by Chunbo Hu on 2018/3/11.
+//  Copyright © 2018年 Chunbo Hu. All rights reserved.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
